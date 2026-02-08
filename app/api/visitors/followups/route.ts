8604f5fecbf704024ff6e6db4053e1523ed769d9
AmrAnter44 <amranter200444@gmail.com>
2026-02-08 11:41:06 +0200

    ت

--- app/api/visitors/followups/route.ts
+++ app/api/visitors/followups/route.ts
@@ -1,6 +1,6 @@
 import { NextResponse } from 'next/server'
 import { prisma } from '../../../../lib/prisma'
-import { verifyAuth } from '../../../../lib/auth'
+import { verifyAuth, requirePermission } from '../../../../lib/auth'
 
 // GET - جلب متابعات زائر معين أو جميع المتابعات
 
